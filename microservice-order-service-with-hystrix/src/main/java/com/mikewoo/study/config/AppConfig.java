93c84baf6a5bcef8c39fc021ee20006f969c0ad3
mingming.gui <ahqsgmm@163.com>
2019-01-22 19:41:07 +0800

    完善服务熔断处理，增加报警机制

--- microservice-order-service-with-hystrix/src/main/java/com/mikewoo/study/config/AppConfig.java
+++ microservice-order-service-with-hystrix/src/main/java/com/mikewoo/study/config/AppConfig.java
@@ -5,6 +5,9 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
 
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 /**
  * @author Eric Gui
  * @date 2019/1/17
@@ -16,5 +19,10 @@ public class AppConfig {
     @LoadBalanced
     public RestTemplate restTemplate() {
         return new RestTemplate();
+    }
+
+    @Bean
+    public ExecutorService msgSenderExecutorService() {
+        return Executors.newCachedThreadPool();
     }
 }
--- microservice-order-service-with-hystrix/src/main/java/com/mikewoo/study/controller/OrderController.java
+++ microservice-order-service-with-hystrix/src/main/java/com/mikewoo/study/controller/OrderController.java
@@ -5,22 +5,34 @@ import com.mikewoo.study.domain.ShoppingList;
 import com.mikewoo.study.service.OrderService;
 import com.mikewoo.study.utils.ResponseResult;
 import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.web.bind.annotation.*;
 
 import java.io.IOException;
 import java.util.Objects;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.TimeUnit;
 
 /**
  * @author Eric Gui
  * @date 2019/1/17
  */
+@Slf4j
 @RestController
 @RequestMapping("/api/v1/order")
 public class OrderController {
 
     @Autowired
     private OrderService orderService;
+
+    @Autowired
+    private StringRedisTemplate redisTemplate;
+
+    @Autowired
+    private ExecutorService msgSenderExecutorService;
 
     @PostMapping("/{userId}")
     @HystrixCommand(fallbackMethod = "saveOrderFailed")
@@ -41,6 +53,17 @@ public class OrderController {
      * @return
      */
     private Object saveOrderFailed(Long userId, ShoppingList shoppingList) {
+        msgSenderExecutorService.submit(() -> {
+            String key = "ORDER:SAVE:FAILED:KEY";
+            String value = redisTemplate.opsForValue().get(key);
+            if (StringUtils.isBlank(value)) {
+                log.info("服务调用失败，发送报警通知");
+                // TODO: 调用短信服务，发送告警短信
+                redisTemplate.opsForValue().set(key, "oder-save-failed", 30, TimeUnit.SECONDS);
+            } else {
+                log.info("服务调用失败，30内不再重复发送告警通知");
+            }
+        });
         return ResponseResult.build(-1, "服务暂时不可用，请稍后重试");
     }
 

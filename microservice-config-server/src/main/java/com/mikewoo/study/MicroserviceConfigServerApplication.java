0cf619da9469a30f11dd2d6d2c74cfe0009ed30c
mingming.gui <ahqsgmm@163.com>
2019-01-24 10:34:38 +0800

    结合github搭建Spring Cloud配置中心组件

---
+++ microservice-config-server/src/main/java/com/mikewoo/study/MicroserviceConfigServerApplication.java
@@ -0,0 +1,16 @@
+package com.mikewoo.study;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@SpringBootApplication
+@EnableConfigServer
+public class MicroserviceConfigServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MicroserviceConfigServerApplication.class, args);
+    }
+
+}
+
